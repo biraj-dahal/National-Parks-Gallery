d65d368d37c417780357dd95639472b8cadc2ef6
biraj-dahal <dahalbiraj10@gmail.com>
2025-02-26 21:11:29 -0500

    Initial Repo

---
+++ National Park Galleries/National_Park_GalleriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  National_Park_GalleriesApp.swift
+//  National Park Galleries
+//
+//  Created by Biraj Dahal on 2/26/25.
+//
+
+import SwiftUI
+
+@main
+struct National_Park_GalleriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
