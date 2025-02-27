58cb921d49cb59ddab0ce4009496bd10246b2f29
biraj-dahal <dahalbiraj10@gmail.com>
2025-02-27 13:55:25 -0500

    Fetch data, and build a simple UI

--- National Park Galleries/ContentView.swift
+++ National Park Galleries/ContentView.swift
@@ -8,17 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var parks: [Park] = []
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+        ScrollView {
+                LazyVStack {
+                    ForEach(parks) { park in
+                        Rectangle()
+                            .aspectRatio(4/3, contentMode: .fit)
+                        
+                                .overlay(alignment: .bottomLeading) {
+                                    Text(park.name)
+                                        .font(.title)
+                                        .bold()
+                                        .foregroundStyle(.white)
+                                        .padding()
+                                }
+                                .cornerRadius(16)
+                                .padding(.horizontal)
+                    }
+                }
+            }
         .padding()
+        .onAppear(perform: {
+            
+            Task {
+                await fetchParks()
+            }
+        })
     }
-}
-
+    
+    
+    private func fetchParks() async {
+        let url = URL(string: "https://developer.nps.gov/api/v1/parks?stateCode=ca&api_key=B8N92518ub5MkqNeyMkciHlqSaSEZwuGgos95sA9")!
+        
+        do{
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            let jsonObjParks = try JSONDecoder().decode(ParksResponse.self, from: data)
+            
+            let parksData = jsonObjParks.data
+            
+            self.parks = parksData
+            
+            
+        } catch{
+            print(error.localizedDescription)
+        }
+        
+    }}
+                  
+        
+            
+            
 #Preview {
     ContentView()
 }
